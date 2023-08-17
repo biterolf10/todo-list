5217a8888d4cf241c8840d7e898732a109589aed
biterolf10 <112725148+biterolf10@users.noreply.github.com>
2023-08-17 10:25:15 +0500

    new version

--- src/App.js
+++ src/App.js
@@ -1,54 +1,44 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Item from './components/Item';
-import Folder from './components/Folder';
-let counterItem = 1
-let counterFolder = 1
+
+let counterItem = 1;
 
 function App() {
-  let [item, setItems] = useState([])
-  let [folder, setFolders] = useState([])
+  const [items, setItems] = useState([]);
 
   function addItem() {
-    let preset = {id: counterItem, title: '', content: ''}
-    let title = document.getElementById('titleItemAdd')
-    let content = document.getElementById('contentAdd')
-    preset.title = title.value
-    preset.content = content.value
+    const content = document.getElementById('contentAdd').value;
+    const numbered = document.getElementById('numberedText').checked;
+    const bigger = document.getElementById('biggerText').checked;
 
-    setItems(item => [...item, preset])
-    counterItem++;
-  }
+    const newItem = {
+      id: counterItem,
+      content: content,
+      bigger: bigger,
+      numbered: numbered
+    };
 
-  function addFolder() {
-    let preset = {id: counterFolder, title: ''}
-    let title = document.getElementById('titleFolderAdd')
-    preset.title = title.value
-
-    setFolders(folder => [...folder, preset])
-    counterFolder++;
+    setItems(prevItems => [...prevItems, newItem]);
+    if (numbered) {
+      counterItem++;
+    };
   }
 
   return (
     <div>
       <div className='Controller'>
         <h2>todo list?</h2>
-        <strong>Add item  </strong>
-        <input id='titleItemAdd' placeholder='Title' type='text'></input>
-        <input id='contentAdd' placeholder='Content' type='text'></input>
-        <button onClick={addItem}>Add</button><br></br>
-        {/*
-        <strong>Add folder  </strong>
-        <input id='titleFolderAdd' placeholder='Title' type='text'></input>
-        <button onClick={addFolder}>Add</button><br></br>
-        */}
+        <label>Add item  </label>
+        <input id='contentAdd' placeholder='Content' type='text'/><br/>
+        <label htmlFor="biggerText">Bold text</label>
+        <input id="biggerText" type='checkbox'/><br/>
+        <label htmlFor="numberedText">Numbered</label>
+        <input id="numberedText" type='checkbox'/><br/><br/>
+        <button onClick={addItem}>Add</button><br/><br/>
       </div>
-      
-      {[folder.map(folder =>
-        <Folder folder={folder} key={folder.id}/>
-      )]}
-      {[item.map(item =>
-        <Item item={item} key={item.id}/>
-      )]}
+      {items.map(item => (
+        <Item item={item} key={item.id} />
+      ))}
     </div>
   );
 }
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,12 +1,38 @@
 import React from 'react';
 
 const Item = (props) => {
-    return (
-        <div className="itemHandler">
-            <strong className="itemComment">{props.item.id}. {props.item.title}</strong>
-            <div className="itemContent">{props.item.content}</div>
-        </div>
-    );
+    if (props.item.bigger === true) {
+        if (props.item.numbered) {
+            return (
+            <div className="itemHandler">
+                <strong className="itemComment">{props.item.id}. {props.item.content}</strong>
+            </div>
+            );
+        } else {
+            return (
+            <ul iv className="itemHandler">
+                <li className="itemComment">{props.item.content}</li>
+            </ul>
+            );
+        }
+        
+    } else {
+        if (props.item.numbered) {
+            return (
+            <div className="itemHandler">
+                <div className="itemComment">{props.item.id}. {props.item.content}</div>
+            </div>
+            );
+        } else {
+            return (
+            <ul iv className="itemHandler">
+                <li className="itemComment">{props.item.content}</li>
+            </ul>
+            );
+        }
+        
+    }
+    
 }
 
 export default Item;
